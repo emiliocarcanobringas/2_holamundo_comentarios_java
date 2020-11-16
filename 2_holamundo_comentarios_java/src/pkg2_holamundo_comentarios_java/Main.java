6c1705f4064e7636dbaa0380060508bc7f1f407e
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-11-15 18:28:23 -0600

    Add files via upload

---
+++ 2_holamundo_comentarios_java/src/pkg2_holamundo_comentarios_java/Main.java
@@ -0,0 +1,23 @@
+/*
+Este programa muestra como insertar comentarios en java, los de una línea
+necesitan una diagonal doble, los de varias línea ocupan una diagonal
+seguida por un asterisco y cierra con un asterisco seguido por una
+diagonal.
+ */
+package pkg2_holamundo_comentarios_java;
+
+/**
+ *
+ * @author Emilio Carcaño Bringas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Estoy orgulloso de pertenecer a Uninaciones y estudiar en la UNAM");//Imprimimos en pantalla
+        System.out.println("Mi nombre es Emilio Carcaño Bringas");//Escribimos: sout y opriminos la tecla tab para escribir: System.out.println
+    }
+    
+}
